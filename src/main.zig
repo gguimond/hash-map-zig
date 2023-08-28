e0db7142c8af00acc023da334e8234dc33f061f2
Guillaume Guimond <guillaume.guimond@orange.com>
2023-08-28 16:28:58 +0200

    finished hashmap

--- src/main.zig
+++ src/main.zig
@@ -36,18 +36,18 @@ fn GHashMap(comptime T: type) type {
         }
 
         fn set(self: *Self, allocator: Allocator, key: []const u8, val: T) !void {
+            std.debug.print("\n set: {d}", .{ val });
             const bucket = self.get_bucket(key);
             var v = self.buckets[bucket];
             while (v) |_v| {
-                var value = _v.*;
-                if(std.mem.eql(u8, value.key, key)) {
-                    std.debug.print("found", .{ });
-                    value.val = val;
-                    std.debug.print("{d}", .{ _v.*.val });
+                if(std.mem.eql(u8, _v.key, key)) {
+                    std.debug.print("\n found", .{ });
+                    _v.val = val;
+                    std.debug.print("\n {d}", .{ _v.val });
                     return;
                 }
-                v = value.next;
-                std.debug.print("{d}", .{ _v.*.val });
+                v = _v.next;
+                std.debug.print("\n {d}", .{ _v.val });
             }
             std.debug.print("oh", .{ });
             var newValue = try allocator.create(Entry(T));
@@ -56,6 +56,51 @@ fn GHashMap(comptime T: type) type {
             newValue.val = val;
             newValue.next = self.buckets[bucket];
             self.buckets[bucket] = newValue;
+        }
+
+        fn get(self: *Self, key: []const u8) ?T {
+            const bucket = self.get_bucket(key);
+            var v = self.buckets[bucket];
+            while (v) |_v| {
+                if(std.mem.eql(u8, _v.key, key)) {
+                    return _v.val;
+                }
+                v = _v.next;
+            }
+            return null;
+        }
+
+        fn delete(self: *Self, allocator: Allocator, key: []const u8) void {
+            const bucket = self.get_bucket(key);
+            var prev: ?*Entry(T) = null;
+            var v = self.buckets[bucket];
+            while (v) |_v| {
+                if(std.mem.eql(u8, _v.key, key)) {
+                    if(prev) |_prev|{
+                        _prev.next = _v.next;
+                    } else {
+                        self.buckets[bucket] = _v.next;
+                    }
+                    allocator.destroy(_v);
+                    return;
+                }
+                prev = _v;
+                v = _v.next;
+            }
+        }
+
+        fn free(self: *Self, allocator: Allocator) void {
+            for(self.buckets) |val| {
+                var v = val;
+                while (v) |_v| {
+                    var next = _v.next;
+                    std.debug.print("\n destroy \n {d}", .{ _v.val });
+                    allocator.destroy(_v);
+                    v = next;
+                }
+            }
+            allocator.free(self.buckets);
+            allocator.destroy(self);
         }
     };
 }
@@ -67,11 +112,25 @@ pub fn main() !void {
     const hashMap = try HashMap.create(alloc);
     std.debug.print("{d}\n", .{ hashMap.nBuckets });
     std.debug.print("{d}\n", .{ HashMap.hash("item a") });
-    std.debug.print("{d}\n", .{ hashMap.get_bucket("item a") });
+    std.debug.print("bucket: {d}\n", .{ hashMap.get_bucket("item a") });
+    std.debug.print("bucket: {d}\n", .{ hashMap.get_bucket("item e") });
+
     try hashMap.set(alloc, "item a", 1);
     try hashMap.set(alloc, "item a", 2);
     try hashMap.set(alloc, "item e", 3);
-    std.debug.print("{d}\n", .{ hashMap.buckets[1].?.*.val });
+    try hashMap.set(alloc, "item z", 7);
+    std.debug.print("\nvalue raw : {d}\n", .{ hashMap.buckets[1].?.*.val });
+    if(hashMap.get("item a")) |val|{
+        std.debug.print("\nvalue a get : {d}\n", .{ val });
+    }
+    if(hashMap.get("item e")) |val|{
+        std.debug.print("\nvalue e get : {d}\n", .{ val });
+    }
+    hashMap.delete(alloc, "item e");
+    if(hashMap.get("item e")) |val|{
+        std.debug.print("\nvalue e get : {d}\n", .{ val });
+    }
+    hashMap.free(alloc);
 }
 
 test "simple test" {
